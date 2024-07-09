fea7c8a24d24a6fb9572f136b9b3537562dc33fc
Versha Rani <vershar1990@gmail.com>
2024-07-09 12:17:28 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirement
+    '''
+    requirement = []
+    with open(file_path) as file_obj:
+        requirement = file_obj.readline()
+        requirement = [req.replace("\n","") for req in requirement]
+
+        if HYPEN_E_DOT in requirement:
+            requirement.remove(HYPEN_E_DOT)
+
+    return requirement
+
+
+
+setup(
+
+    name = "mlproject",
+    version= "0.0.1",
+    author="Versha",
+    author_email="vershar1990@gmail.com",
+    description= "End to End ML project",
+    long_description= open("README.md").read(),
+    packages=find_packages(),
+    install_requires = get_requirements("requirements.txt")
+
+)
